5bff3043504c86209b233069ee552c719a573e43
ldevernay <boblehamster@yahoo.fr>
2019-03-11 15:16:41 +0100

    Added Eau & GES to results

--- popup/results.js
+++ popup/results.js
@@ -20,11 +20,14 @@ window.onload = function() {
 	var html ="<table class=\"table table-condensed\"> <tbody>";
 	html+="<tr><td>Url</td><td> " + url + "</td></tr>";
 	html+="<tr><td>Nombre de  requêtes </td><td>" + localStorage.getItem('nb_request')  + "</td></tr>";
-	html+="<tr><td>Taille   </td><td>" + localStorage.getItem('byte_total')/1000 + " KBytes </td></tr>"
+	html+="<tr><td>Taille   </td><td>" + localStorage.getItem('byte_total')/1000 + " KBytes </td></tr>";
 	html+="<tr><td>Taille du DOM  </td><td>" + localStorage.getItem("dom_size") + "</td></tr>";
+    html+="<tr><td>GES  </td><td>" + localStorage.getItem("ges") + "</td></tr>";
+    html+="<tr><td>Eau  </td><td>" + localStorage.getItem("water") + "</td></tr>";
 	html+="<tr><td>Eco Index </td><td>" + eco_index + "</td></tr>";
 	html+='<tr><td>Note </td><td> <span class="note ' + note +'">' + note + '</span></td></tr></tbody></table>';
-console.log("html=" + html);
+	
+	console.log("html=" + html);
 	// newDiv.innerHTML =html;
 	
 	document.getElementById("result").innerHTML=html;
